8ee88eaa30ab7c824a2a390ba346b006b47ad32e
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-04-03 22:22:51 +0900

    Java から複数のタスクを渡してみよう

--- src/main/java/com/example/todo/controller/tasks/TaskController.java
+++ src/main/java/com/example/todo/controller/tasks/TaskController.java
@@ -5,14 +5,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/tasks")
 public class TaskController {
 
     @GetMapping
     public String index(Model model) {
-        var task = new TaskDTO(1, "Spring Boot を学ぶ", "TODO アプリケーションを作る", "TODO");
-        model.addAttribute("task", task);
+        var task1 = new TaskDTO(1, "Spring Boot を学ぶ", "TODO アプリケーションを作る", "TODO");
+        var task2 = new TaskDTO(2, "Spring Security を学ぶ", "ログイン機能を作る", "TODO");
+        var dtoList = List.of(task1, task2);
+        model.addAttribute("taskList", dtoList);
         return "tasks/list";
     }
 }

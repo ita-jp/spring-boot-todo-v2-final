47851f74152a9fbdd19db146015719205956b7a3
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-04-04 22:34:32 +0900

    Service層とRepository層を実装しよう

--- src/main/java/com/example/todo/repository/TaskRepository.java
+++ src/main/java/com/example/todo/repository/TaskRepository.java
@@ -2,9 +2,11 @@ package com.example.todo.repository;
 
 import com.example.todo.service.tasks.TaskEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface TaskRepository {
@@ -12,4 +14,6 @@ public interface TaskRepository {
     @Select("SELECT id, summary, description, status FROM tasks")
     List<TaskEntity> select();
 
+    @Select("SELECT id, summary, description, status FROM tasks WHERE id = #{id}")
+    Optional<TaskEntity> selectById(@Param("id") long id);
 }

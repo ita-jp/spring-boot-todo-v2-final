948c439b40c2fc049480f6ebebd833d928a3439a
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-04-04 22:29:40 +0900

    @PathVariable を使ってURLに含まれるタスクIDを取得しよう

--- src/main/java/com/example/todo/controller/tasks/TaskController.java
+++ src/main/java/com/example/todo/controller/tasks/TaskController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,8 +25,8 @@ public class TaskController {
         return "tasks/list";
     }
 
-    @GetMapping("detail")
-    public String showDetail() {
+    @GetMapping("{id}")
+    public String showDetail(@PathVariable("id") long id) {
         return "tasks/detail";
     }
 }

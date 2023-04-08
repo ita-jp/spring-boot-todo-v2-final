5195766645a39972108b059546268417501d4fd5
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-04-08 22:24:50 +0900

    作成のリクエストを受けるハンドラーメソッドを実装しよう

--- src/main/java/com/example/todo/controller/tasks/TaskController.java
+++ src/main/java/com/example/todo/controller/tasks/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -37,4 +38,10 @@ public class TaskController {
     public String showCreationForm() {
         return "tasks/form";
     }
+
+    @PostMapping
+    public String create(Model model) {
+        return index(model);
+    }
+
 }

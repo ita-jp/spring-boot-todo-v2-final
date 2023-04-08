697a2a2d0645f0869d1cb935971ec359fba912ad
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-04-08 22:38:24 +0900

    リファクタリング：TaskForm#toEntity を導入しよう

--- src/main/java/com/example/todo/controller/tasks/TaskForm.java
+++ src/main/java/com/example/todo/controller/tasks/TaskForm.java
@@ -1,8 +1,20 @@
 package com.example.todo.controller.tasks;
 
+import com.example.todo.service.tasks.TaskEntity;
+import com.example.todo.service.tasks.TaskStatus;
+
 public record TaskForm(
         String summary,
         String description,
         String status
 ) {
+
+    public TaskEntity toEntity() {
+        return new TaskEntity(
+                null,
+                summary,
+                description,
+                TaskStatus.valueOf(status)
+        );
+    }
 }

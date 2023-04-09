50073b50ee7c045993193c67d3b00c058d8cb7bf
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-04-09 23:38:04 +0900

    ステータスに入力できる文字列をバリデーションで制限しよう

--- src/main/java/com/example/todo/controller/tasks/TaskForm.java
+++ src/main/java/com/example/todo/controller/tasks/TaskForm.java
@@ -3,6 +3,7 @@ package com.example.todo.controller.tasks;
 import com.example.todo.service.tasks.TaskEntity;
 import com.example.todo.service.tasks.TaskStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record TaskForm(
@@ -11,6 +12,7 @@ public record TaskForm(
         String summary,
         String description,
         @NotBlank
+        @Pattern(regexp = "TODO|DOING|DONE", message = "Todo, Doing, Done のいずれかを指定してください")
         String status
 ) {
 

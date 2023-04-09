05b0dc4c11a798a5f37597df4636637c2473616c
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-04-09 23:30:52 +0900

    最大長のバリデーションを追加しよう

--- src/main/java/com/example/todo/controller/tasks/TaskForm.java
+++ src/main/java/com/example/todo/controller/tasks/TaskForm.java
@@ -3,9 +3,11 @@ package com.example.todo.controller.tasks;
 import com.example.todo.service.tasks.TaskEntity;
 import com.example.todo.service.tasks.TaskStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record TaskForm(
         @NotBlank
+        @Size(max = 256, message = "256文字以内で入力してください")
         String summary,
         String description,
         @NotBlank
